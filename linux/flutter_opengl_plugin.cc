af0573791dcd68f348c8c219e7db12fdfc7e3a12
sneurlax <sneurlax@gmail.com>
2024-04-07 11:02:14 -0500

    feat: move GLEW init to after GL context creation in createSurface

--- linux/flutter_opengl_plugin.cc
+++ linux/flutter_opengl_plugin.cc
@@ -18,11 +18,26 @@
 #include "include/fl_my_texture_gl.h"
 #include "../src/ffi.h"
 #include "../src/common.h"
-#include "include/test.h"
+
 
 G_DEFINE_TYPE(FlutterOpenglPlugin, flutter_opengl_plugin, g_object_get_type())
 
 #define EGL_EGLEXT_PROTOTYPES
+
+static bool glew_initialized = false;
+
+static bool ensure_glew_init() {
+	if (glew_initialized)
+		return true;
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cout << "glewInit error: " << glewGetErrorString(err) << std::endl;
+		return false;
+	}
+	glew_initialized = true;
+	return true;
+}
 
 // Called when a method call is received from Flutter.
 static void flutter_opengl_plugin_handle_method_call(
@@ -32,12 +47,8 @@ static void flutter_opengl_plugin_handle_method_call(
 	g_autoptr(FlMethodResponse) response = nullptr;
 
 	const gchar *method = fl_method_call_get_name(method_call);
-	// Get Dart arguments
 	FlValue *args = fl_method_call_get_args(method_call);
 
-	/********************************************/
-	/*** CREATE SURFACE *************************/
-	/********************************************/
 	if (strcmp(method, "createSurface") == 0)
 	{
 		int width = 0;
@@ -58,11 +69,10 @@ static void flutter_opengl_plugin_handle_method_call(
 		else
 		{
 			if (self->context != nullptr &&
+				self->myTexture != nullptr &&
 				self->myTexture->width == width &&
 				self->myTexture->height == height)
 			{
-				// g_autoptr(FlValue) result = fl_value_new_int(-1);
-				// response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
 				fl_texture_registrar_unregister_texture(self->texture_registrar, self->texture);
 				if (getRenderer() != nullptr)
 					stopThread();
@@ -72,8 +82,26 @@ static void flutter_opengl_plugin_handle_method_call(
 			window = gtk_widget_get_parent_window(GTK_WIDGET(self->fl_view));
 			GError *error = NULL;
 			self->context = gdk_window_create_gl_context(window, &error);
+			if (self->context == nullptr) {
+				std::cout << "Failed to create GL context: "
+				          << (error ? error->message : "unknown") << std::endl;
+				if (error) g_error_free(error);
+				response = FL_METHOD_RESPONSE(fl_method_error_response_new(
+					"101", "Failed to create GL context", nullptr));
+				fl_method_call_respond(method_call, response, nullptr);
+				return;
+			}
 
 			gdk_gl_context_make_current(self->context);
+
+			// Initialize GLEW now that we have a current GL context
+			if (!ensure_glew_init()) {
+				gdk_gl_context_clear_current();
+				response = FL_METHOD_RESPONSE(fl_method_error_response_new(
+					"102", "Failed to initialize GLEW", nullptr));
+				fl_method_call_respond(method_call, response, nullptr);
+				return;
+			}
 
 			glGenTextures(1, &self->texture_name);
 			glBindTexture(GL_TEXTURE_2D, self->texture_name);
@@ -98,7 +126,7 @@ static void flutter_opengl_plugin_handle_method_call(
 			response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
 		}
 
-	} // end createSurface
+	}
 	else
 	{
 		response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
@@ -117,7 +145,15 @@ static void flutter_opengl_plugin_class_init(FlutterOpenglPluginClass *klass)
 	G_OBJECT_CLASS(klass)->dispose = flutter_opengl_plugin_dispose;
 }
 
-static void flutter_opengl_plugin_init(FlutterOpenglPlugin *self) {}
+static void flutter_opengl_plugin_init(FlutterOpenglPlugin *self)
+{
+	self->context = nullptr;
+	self->texture_registrar = nullptr;
+	self->myTexture = nullptr;
+	self->texture_name = 0;
+	self->texture = nullptr;
+	self->fl_view = nullptr;
+}
 
 static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
 						   gpointer user_data)
@@ -146,16 +182,6 @@ void flutter_opengl_plugin_register_with_registrar(FlPluginRegistrar *registrar)
 		method_call_cb,
 		g_object_ref(plugin),
 		g_object_unref);
-
-	// Initialize GL
-	glewExperimental = GL_TRUE;
-	GLenum err = glewInit();
-	if (GLEW_OK != err)
-	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		std::cout << "Error: " << glewGetErrorString(err) << std::endl;
-		return;
-	}
 
 	g_object_unref(plugin);
 }
