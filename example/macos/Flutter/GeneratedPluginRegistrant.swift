682ffcf1fd78737b4fd05cb484de5c835aedab08
sneurlax <sneurlax@gmail.com>
2024-07-13 08:53:02 -0500

    chore: update example macOS project for plugin integration

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import flutter_opengl
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterOpenglPlugin.register(with: registry.registrar(forPlugin: "FlutterOpenglPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
