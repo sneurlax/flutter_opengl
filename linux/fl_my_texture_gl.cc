e118ba334ce673725a037121ad5ee6bf0ef0348e
sneurlax <sneurlax@gmail.com>
2025-12-29 22:32:31 -0600

    feat: switch Linux texture from FlTextureGL to FlPixelBufferTexture

--- linux/fl_my_texture_gl.cc
+++ linux/fl_my_texture_gl.cc
@@ -1,26 +1,23 @@
 #include "include/fl_my_texture_gl.h"
 
-// https://github.com/lattice0/external_texture_flutter_linux/tree/master/external_texture/linux
-
 #include <iostream>
+#include <cstring>
+
 G_DEFINE_TYPE(FlMyTextureGL,
               fl_my_texture_gl,
-              fl_texture_gl_get_type())
+              fl_pixel_buffer_texture_get_type())
 
-static gboolean fl_my_texture_gl_populate(FlTextureGL *texture,
-                                          uint32_t *target,
-                                          uint32_t *name,
-                                          uint32_t *width,
-                                          uint32_t *height,
-                                          GError **error)
+static gboolean fl_my_texture_gl_copy_pixels(FlPixelBufferTexture *texture,
+                                              const uint8_t **out_buffer,
+                                              uint32_t *width,
+                                              uint32_t *height,
+                                              GError **error)
 {
-  // std::cout << "attention: populate called" << std::endl;
-  FlMyTextureGL* f = (FlMyTextureGL*) texture;
-  *target = f->target;
-  *name = f->name;
+  FlMyTextureGL* f = FL_MY_TEXTURE_GL(texture);
+  *out_buffer = f->buffer;
   *width = f->width;
   *height = f->height;
-  return true;
+  return TRUE;
 }
 
 FlMyTextureGL *fl_my_texture_gl_new(uint32_t target,
@@ -33,16 +30,27 @@ FlMyTextureGL *fl_my_texture_gl_new(uint32_t target,
   r->name = name;
   r->width = width;
   r->height = height;
+  r->buffer = new uint8_t[width * height * 4]();
   return r;
+}
+
+static void fl_my_texture_gl_dispose(GObject *object)
+{
+  FlMyTextureGL *self = FL_MY_TEXTURE_GL(object);
+  delete[] self->buffer;
+  self->buffer = nullptr;
+  G_OBJECT_CLASS(fl_my_texture_gl_parent_class)->dispose(object);
 }
 
 static void fl_my_texture_gl_class_init(
     FlMyTextureGLClass *klass)
 {
-  FL_TEXTURE_GL_CLASS(klass)->populate =
-      fl_my_texture_gl_populate;
+  FL_PIXEL_BUFFER_TEXTURE_CLASS(klass)->copy_pixels =
+      fl_my_texture_gl_copy_pixels;
+  G_OBJECT_CLASS(klass)->dispose = fl_my_texture_gl_dispose;
 }
 
 static void fl_my_texture_gl_init(FlMyTextureGL *self)
 {
+  self->buffer = nullptr;
 }
