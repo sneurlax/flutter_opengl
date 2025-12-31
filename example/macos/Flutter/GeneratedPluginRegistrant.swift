b5b8ef7af389354319f0a0179c037d1324fadc93
sneurlax <sneurlax@gmail.com>
2025-12-30 18:28:25 -0600

    chore: update generated macOS plugin registrant after removing stubs

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_opengl
+import file_picker
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterOpenglPlugin.register(with: registry.registrar(forPlugin: "FlutterOpenglPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
