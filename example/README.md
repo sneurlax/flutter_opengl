4a9265e1467be11fc2df29576fe55356af79f43f
sneurlax <sneurlax@gmail.com>
2026-01-11 10:05:00 -0600

    chore: remove stale test files, fix broken tests, clean boilerplate

--- example/README.md
+++ example/README.md
@@ -1,16 +1,9 @@
-# flutter_opengl_example
+# flutter_opengl example
 
-Demonstrates how to use the flutter_opengl plugin.
+Demonstrates flutter_opengl rendering a ShaderToy shader on all supported platforms.
 
-## Getting Started
+## Running
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```bash
+flutter run -d linux    # or chrome, windows, macos, android, ios
+```
--- test/flutter_opengl_test.dart
+++ test/flutter_opengl_test.dart
@@ -2,26 +2,10 @@ import 'package:flutter_opengl/src/flutter_opengl_method_channel.dart';
 import 'package:flutter_opengl/src/flutter_opengl_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// class MockFlutterOpenglPlatform
-//     with MockPlatformInterfaceMixin
-//     implements FlutterOpenglPlatform {
-//
-//   @override
-//   Future<String?> getPlatformVersion() => Future.value('42');
-// }
-
 void main() {
   final FlutterOpenglPlatform initialPlatform = FlutterOpenglPlatform.instance;
 
   test('$MethodChannelFlutterOpengl is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterOpengl>());
   });
-
-  // test('getPlatformVersion', () async {
-  //   FlutterOpengl flutterOpenglPlugin = FlutterOpengl();
-  //   MockFlutterOpenglPlatform fakePlatform = MockFlutterOpenglPlatform();
-  //   FlutterOpenglPlatform.instance = fakePlatform;
-  //
-  //   expect(await flutterOpenglPlugin.getPlatformVersion(), '42');
-  // });
 }
